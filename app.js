68c9ab23d315dab910e3bd9292bfaf86006243f4
Andrej062 <andrej.gluchariov@gmail.com>
2025-10-14 13:42:52 +0200

    сс

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const app = express();
 
+const path = require('path');
 const Database = require('better-sqlite3');
-const db = new Database('cloneK.db');
+
+const dbPath = path.join(__dirname, 'cloneK.db');
+const db = new Database(dbPath);
+
+console.log('Using database file:', dbPath);
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('/member', (req, res) =>{
-    const row =db.prepare('SELECT * FROM member').all();
-    res.json(row);
+app.get('/member', (req, res) => {
+    const rows = db.prepare('SELECT * FROM member').all();
+    res.json(rows);
 });
 
-app.listen(3000, ()=> {
+app.listen(3000, () => {
     console.log('Server is on port 3000');
 });
